6dc85181a6af145cedb3f48a3a92e55c566152a1
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-02-15 12:03:12 +0300

    Добавьте файлы проекта.

---
+++ NSLocalMaximum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NSLocalMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[30];
+            int randomMin = 0;
+            int randomMax = 10;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(randomMin, randomMax);
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i == 0 && numbers[i] > numbers[i + 1])
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+
+                if (i > 0 && i < numbers.Length - 1)
+                {
+                    if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+                    {
+                        Console.Write(numbers[i] + " ");
+                    }
+                }
+
+                if (i == numbers.Length - 1 && numbers[i] > numbers[i - 1])
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
